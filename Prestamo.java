ef396040037c03d08caa2e34896c89f6f77a2cba
matiasah2117 <matias.ah2117@gmail.com>
2026-08-25 23:14:44 -0400

    Add files via upload

---
+++ Prestamo.java
@@ -0,0 +1,26 @@
+/* package POO-2026-Biblioteca;*/
+
+// Omitimos la línea del package por ahora para no tener problemas al compilar por consola
+
+public class Prestamo {
+
+    // --- ATRIBUTOS ---
+    Libro libro; // El libro prestado (objeto completo)[cite: 1]
+    Socio socio; // Quién lo pidió (objeto completo)[cite: 1]
+    String fechaPrestamo; // Ejemplo: "2026-08-27"[cite: 1]
+    boolean devuelto; // true si ya se devolvió, false si sigue prestado[cite: 1]
+
+    // --- MÉTODOS ---
+
+    // Método para registrar la devolución
+    void registrarDevolucion() {
+        devuelto = true; // 1) deja "devuelto" en true[cite: 1]
+        libro.devolver(); // 2) le avisa al libro que ya está disponible de nuevo[cite: 1]
+    }
+
+    // Método para mostrar la información en consola
+    void mostrarInfo() {
+        // Imprime: "Préstamo: [titulo del libro] a [nombre del socio] el [fecha] devuelto: [si/no]"[cite: 1]
+        System.out.println("Préstamo: " + libro.titulo + " a " + socio.nombre +  " el " + fechaPrestamo + " devuelto: " + devuelto);[cite: 1]
+    }
+}
